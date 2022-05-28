ee27fd314e40d9510199d6e570b38efd44ad0d37
Jhfnetboy(Survivor) <jhfnetboy@gmail.com>
2022-05-28 16:07:57 +0800

    ugly tab ok

--- src/components/MintNFTInput.js
+++ src/components/MintNFTInput.js
@@ -1,16 +1,50 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-const useStyles = makeStyles({
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
   },
-});
-
+}));
 
 
 const MintNFTInput = ({...props}) => {
@@ -26,22 +60,30 @@ const MintNFTInput = ({...props}) => {
     return (
     <div>
     <hr/>
+    {/* tab begin  */}
 
-<Paper className={classes.root}>
-  <Tabs
-    value={value}
-    onChange={handleChange}
-    indicatorColor="primary"
-    textColor="primary"
-    centered
-  >
-    <Tab label="Item One" />
-    <Tab label="Item Two" />
-    <Tab label="Item Three" />
-  </Tabs>
-</Paper>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </div>
 
-{/* content */}
+    {/* tab end */}
+
+{/* content begin */}
      
         <p>  <br />
         <a className="sub-text gradient-text"> Build a Soul Bound Realtions:</a> <br />       
@@ -87,7 +129,7 @@ const MintNFTInput = ({...props}) => {
         >
           Mint NFT
         </button>
-{/* content */}
+{/* content end*/}
 
 
 
